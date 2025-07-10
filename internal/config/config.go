bc9a55187d06dc567b5f7cec18bb174154110970
Ikramullah <ikramullah@mplus.software>
2025-07-10 12:39:36 +0700

    refactor(config): remove unnecessary configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,19 +15,19 @@ type BobbitConfig struct {
 
 func New() BobbitConfig {
 	return BobbitConfig{
-		FifoPath:   lib.GetDefaultEnv("BOBBIT_FIFO_PATH", "/tmp/bobbitd.fifo"),
 		SocketPath: lib.GetDefaultEnv("BOBBIT_SOCKET_PATH", "/tmp/bobbitd.sock"),
 		DataDir:    lib.GetDefaultEnv("BOBBIT_DATA_DIR", "/tmp/bobbitd"),
-		Separator:  lib.GetDefaultEnv("BOBBIT_SEPARATOR", "---"),
 	}
 }
 
 func (c BobbitConfig) GetLockfilePath(id string) string {
 	return filepath.Join(c.DataDir, id+".lock")
 }
+
 func (c BobbitConfig) GetLogfilePath(id string) string {
 	return filepath.Join(c.DataDir, id+".log")
 }
+
 func (c BobbitConfig) GetExitCodePath(id string) string {
 	return filepath.Join(c.DataDir, id+".exitcode")
 }
