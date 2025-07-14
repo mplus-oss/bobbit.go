71b464fb6f841e6d17a776a83d57f45b0cbc474e
Ikramullah <ikramullah@mplus.software>
2025-07-14 10:23:48 +0700

    refactor(daemon): change routing from if statement to switch

--- cmd/bobbitd/routing.go
+++ cmd/bobbitd/routing.go
@@ -8,19 +8,16 @@ import (
 )
 
 func RouteHandler(d *daemon.DaemonStruct, jc *daemon.JobContext) {
-	if jc.Payload.Request == payload.REQUEST_EXECUTE_JOB {
+	switch r := jc.Payload.Request; r {
+	case payload.REQUEST_EXECUTE_JOB:
 		if err := d.HandleJob(jc); err != nil {
 			log.Println(err)
 		}
-		return
-	}
-
-	if jc.Payload.Request == payload.REQUEST_LIST {
+	case payload.REQUEST_LIST:
 		if err := d.ListJob(jc); err != nil {
 			log.Println(err)
 		}
-		return
+	default:
+		log.Printf("WARNING: Outbound request: %+v", jc)
 	}
-
-	log.Printf("WARNING: Outbound request: %+v", jc)
 }
