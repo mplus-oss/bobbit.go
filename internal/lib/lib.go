e5a7fd1a310d70ca33856c7a7e2710353ff26445
Ikramullah Latif <ikramullah@mplus.software>
2025-12-26 11:30:53 +0700

    feat(lib): add HumanizeDuration helper function

--- internal/lib/lib.go
+++ internal/lib/lib.go
@@ -4,7 +4,10 @@ import (
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
+	"math"
 	"os"
+	"time"
 )
 
 func GetDefaultEnv(envVar string, value string) string {
@@ -25,4 +28,29 @@ func GenerateRandomHash(size int) (string, error) {
 	hasher.Write(randomBytes)
 	fullHash := hex.EncodeToString(hasher.Sum(nil))
 	return fullHash[:size], nil
+}
+
+func HumanizeDuration(duration time.Duration) string {
+	if duration.Seconds() < 60.0 {
+		return fmt.Sprintf("%d seconds", int64(duration.Seconds()))
+	}
+	if duration.Minutes() < 60.0 {
+		remainingSeconds := math.Mod(duration.Seconds(), 60)
+		return fmt.Sprintf("%d minutes %d seconds", int64(duration.Minutes()), int64(remainingSeconds))
+	}
+	if duration.Hours() < 24.0 {
+		remainingMinutes := math.Mod(duration.Minutes(), 60)
+		remainingSeconds := math.Mod(duration.Seconds(), 60)
+		return fmt.Sprintf("%d hours %d minutes %d seconds",
+			int64(duration.Hours()), int64(remainingMinutes), int64(remainingSeconds),
+		)
+	}
+
+	remainingHours := math.Mod(duration.Hours(), 24)
+	remainingMinutes := math.Mod(duration.Minutes(), 60)
+	remainingSeconds := math.Mod(duration.Seconds(), 60)
+	return fmt.Sprintf("%d days %d hours %d minutes %d seconds",
+		int64(duration.Hours()/24), int64(remainingHours),
+		int64(remainingMinutes), int64(remainingSeconds),
+	)
 }
